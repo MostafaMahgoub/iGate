3e0d20ab751ec8f48317aa53f66c0407799103fc
MostafaMahgoub <mostafamahgoub@outlook.com>
2025-06-01 18:41:59 +0300

    feat : implemented initial structure needed for product listing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["i.dummyjson.com", "dummyjson.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.dummyjson.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "dummyjson.com",
+        pathname: "**",
+      },
+    ],
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
